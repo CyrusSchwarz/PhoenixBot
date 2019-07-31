c957ac63744e4fdb6974f9a02f728ba1b34509f6
Cyrus <46934684+CyrusSchwarz@users.noreply.github.com>
2019-07-31 07:06:06 -0700

    Fixed the bug where a space would create a new line

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,83 +5,47 @@
 
 
 /* TODO
- * make it so that there's not a new line every space
+ * 
  * 
  * */
 
 
-const Discord = require("discord.js");
 
-function randomIntInc(low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low);
-}
+
+const Discord = require("discord.js");
 
 
 
 module.exports.run = async (bot, message, args) => {
 
-        //var rnd = randomIntInc(1, 5);
-    var rnd = 1;
-    if (rnd === 1) {
 
-        let botembed = new Discord.RichEmbed()
-            
-            .addField('Your Question', args) //Displays the user's question
-            .addField('My answer', 'lolno') //Gives answer
-         
-        
+    var fortunes = [
+        "`Absolutely`",
+        "`lolno`",
+        "`wtf`",
+        "`ok and`",
+        "`Yes`",
+        "`No`",
+        "`uh`",
+        "`what`"
+    ];
 
-            .setColor(0x81ff14) //Make the embed cyrusgreen
-            .setFooter('Searched User') //Add a footer
-            .setTimestamp() //Timestamp the footer
-        message.channel.send(botembed);
-    }
 
-       
-    if (rnd === 2) {
 
-        let botembed = new Discord.RichEmbed()
-            .setDescription("the AI knows all")
-            .setColor("#81ff14")
-            .setField(args , true)
-            .setField('wtf')
-            .setTimestamp()
-        message.channel.send(botembed);
-    }
 
-    if (rnd === 3) {
-
-        let botembed = new Discord.RichEmbed()
-            .setDescription("the AI knows all")
-            .setColor("#81ff14")
-            .setField(args , true)
-            .setField('absolutely')
-            .setTimestamp()
-        message.channel.send(botembed);
-    }
-    if (rnd === 4) {
-
-        let botembed = new Discord.RichEmbed()
-            .setDescription("the AI knows all")
-            .setColor("#81ff14")
-            .setField(args , true)
-            .setField('no?')
-            .setTimestamp()
-        message.channel.send(botembed);
-    }
-
-    if (rnd === 5) {
-
-        let botembed = new Discord.RichEmbed()
-            .setDescription("the AI knows all")
-            .setColor("#81ff14")
-            .setField(args , true)
-            .setField('okay and')
-            .setTimestamp()
-        message.channel.send(botembed);
-    }
-    
+    let answer = fortunes[~~(Math.random() * fortunes.length)];
+    let botembed = new Discord.RichEmbed()
+        .setColor("#81ff14")
+        .setTitle('8ball')
+        .setThumbnail('http://www.myiconfinder.com/uploads/iconsets/256-256-7a291c57d6ee238eb87c9515e5f99bf9.png')
+        .addField('Your question', args.join(" "))
+        .addField('My Answer', answer)
+        .setTimestamp()
+        .setFooter('8ball');
+    message.channel.send(botembed);
 };
+
+
 
 module.exports.help = {
     name: '8ball'
