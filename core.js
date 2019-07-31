44601da040c24c9d6b680638f17d606d4a798f4c
Cyrus <46934684+CyrusSchwarz@users.noreply.github.com>
2019-07-31 07:06:31 -0700

    added the logging of servers and a join message

--- core.js
+++ core.js
@@ -5,6 +5,12 @@ const token = require("./token.json");
 const bot = new Discord.Client({ disableEveryone: true });
 bot.commands = new Discord.Collection();
 
+
+
+
+
+
+
 fs.readdir("./commands/", (err, files) => {
 
     if (err) console.log(err);
@@ -38,7 +44,7 @@ bot.on('ready', () => {
 //Says says the bot has been initialized in the server.
 bot.on('ready', () => {
     var generalChannel = bot.channels.get("563847744930709504") // Replace with known channel ID
-    bot.user.setActivity(bot.guilds.size + " servers in Cyrus's basement | Do ;help for help", { type: "WATCHING" });
+    bot.user.setActivity(bot.guilds.size + " servers in Cyrus's basement", { type: "WATCHING" });
 });
 
 bot.on("message", async message => {
@@ -60,5 +66,33 @@ bot.on("message", async message => {
 
 
 });
+
+bot.on("guildCreate", guild => {
+   
+    console.log(`The bot just joined to ${guild.name}, Owned by ${guild.owner.user.tag}`);
+ 
+
+    var guildMSG = guild.channels.find('name', 'general');
+
+    if (guildMSG) {
+        guildMSG.send(`
+This bot was created by **cyrus#6076**\n\
+If you do not have embeds allowed for the bot, it will not work properly!
+To see the features I have planned and am working on, look here: https://trello.com/b/nz0EOcMX/phoenix-bot
+Here is an invite to the support server: https://discord.gg/6n6B3Gg
+`);
+    } else {
+        return;
+    }
+});
+
+// Logs of the bot leaves a server and changed the game of the bot
+bot.on("guildDelete", guild => {
+
+    console.log(`The bot has been left ${guild.name}, Owned by ${guild.owner.user.tag}`);
+    
+});
+
+
 
 bot.login(token.token);
