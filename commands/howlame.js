691fab9697ccbf7c60c7594987ce0c7d616817d7
Cyrus <46934684+CyrusSchwarz@users.noreply.github.com>
2019-08-02 17:16:45 -0700

    new command

---
+++ commands/howlame.js
@@ -0,0 +1,67 @@
+﻿//Author: Cyrus Schwarz
+//Date: 8/2/19
+//Category: Fun
+//Purpose: tells you how lame you are lol
+
+
+/* TODO
+ * 
+ * 
+ * */
+
+
+
+
+const Discord = require("discord.js");
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+
+    var percentages = [
+        "`0%`",
+        "`10%`",
+        "`20%`",
+        "`25%`",
+        "`50%`",
+        "`60%`",
+        "`69%`",
+        "`75%`",
+        "`99.9%`",
+        "`100%`"
+    ];
+
+
+
+    if (args[0]) {
+        let answer = percentages[~~(Math.random() * percentages.length)];
+        let botembed = new Discord.RichEmbed()
+            .setColor("#81ff14")
+            .setTitle('howlame')
+            .setThumbnail('http://truestar.life/wp-content/uploads/2016/07/lame.png')
+            .addField('AI lame rating', args + ' is' + answer + ' lame')
+            .setTimestamp()
+            .setFooter('howlame');
+        message.channel.send(botembed);
+    } else {
+        let answer = percentages[~~(Math.random() * percentages.length)];
+        let botembed = new Discord.RichEmbed()
+            .setColor("#81ff14")
+            .setTitle('howlame')
+            .setThumbnail('http://truestar.life/wp-content/uploads/2016/07/lame.png')
+            .addField('AI lame rating', 'You are ' + answer + ' lame')
+            .setTimestamp()
+            .setFooter('howlame');
+        message.channel.send(botembed);
+
+
+    }
+
+};
+
+
+
+module.exports.help = {
+    name: 'howlame'
+}
